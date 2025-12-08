2f30cdf6c69176fa0222e9b783aed5d41d3c1886
Ryan Lynch <lynchryan2@gmail.com>
2025-12-08 19:51:33 +0000

    Adding prettier and tailwind.

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+    tabWidth: 4,
+    useTabs: true,
+    semi: true,
+    singleQuote: false,
+    trailingComma: 'all',
+    plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default config;
