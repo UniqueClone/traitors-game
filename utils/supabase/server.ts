83d8e9d1d3abe8f3eb0e3b7ae1abe3e0b3feccea
Ryan Lynch <lynchryan2@gmail.com>
2025-12-10 19:02:32 +0000

    Cleaning server.ts and fixing async errors

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,4 +1,4 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -7,19 +7,15 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
     return createServerClient(supabaseUrl!, supabaseKey!, {
         cookies: {
-            getAll() {
-                return cookieStore.getAll();
+            async getAll() {
+                return (await cookieStore).getAll();
             },
             setAll(cookiesToSet) {
                 try {
-                    cookiesToSet.forEach(({ name, value, options }) =>
-                        cookieStore.set(name, value, options),
+                    cookiesToSet.forEach(async ({ name, value, options }) =>
+                        (await cookieStore).set(name, value, options),
                     );
-                } catch {
-                    // The `setAll` method was called from a Server Component.
-                    // This can be ignored if you have middleware refreshing
-                    // user sessions.
-                }
+                } catch {}
             },
         },
     });
